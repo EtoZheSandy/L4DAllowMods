c7f1264939372e46ed54311824683e2c9d27a9ea
Sandy <82713915+EtoZheSandy@users.noreply.github.com>
2024-09-29 01:17:52 +0300

    Добавил Roboto font

--- composeApp/src/desktopMain/kotlin/su/afk/l4d2/presenter/view/theme/Theme.kt
+++ composeApp/src/desktopMain/kotlin/su/afk/l4d2/presenter/view/theme/Theme.kt
@@ -2,9 +2,16 @@ package com.example.compose
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Typography
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import kotlinproject.composeapp.generated.resources.Res
+import kotlinproject.composeapp.generated.resources.Roboto_Bold
+import kotlinproject.composeapp.generated.resources.Roboto_Medium
+import org.jetbrains.compose.resources.Font
 
 
 private val lightScheme = lightColors(
@@ -39,14 +46,20 @@ fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
-  val colorScheme = when {
-      darkTheme -> darkScheme
-      else -> lightScheme
-  }
+    val colorScheme = when {
+        darkTheme -> darkScheme
+        else -> lightScheme
+    }
 
-  MaterialTheme(
-    colors = colorScheme,
-    content = content,
-  )
+    MaterialTheme(
+        colors = colorScheme,
+        content = content,
+        typography = Typography(
+            defaultFontFamily = FontFamily(
+                Font(Res.font.Roboto_Medium, FontWeight.Normal),
+                Font(Res.font.Roboto_Bold, FontWeight.Bold)
+            )
+        ),
+    )
 }
 
